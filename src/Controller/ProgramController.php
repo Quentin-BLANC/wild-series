3e6988a7c78fce2f149bfde505a668c8ee4e1d0a
Quentin-BLANC <quentinblanc587@gmail.com>
2021-01-11 22:37:12 +0100

    08 - Symfony : Récupérer des données stockées avec Doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,37 +1,57 @@
 <?php
 
-// src/Controller/ProgramController.php
-
 namespace App\Controller;
 
+use App\Entity\Program;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/programs/", name="program_")
+ * @Route("/programs", name="program_")
  */
 class ProgramController extends AbstractController
 
 {
     /**
-     * @Route("", name="index")
+     * Show all rows from Program’s entity [ toutes les lignes de l'entité du programme] 
+     * 
+     * @Route("/", name="index")
+     * @return Response A response instance
      */
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series']);
+        $programs = $this->getDoctrine()
+             ->getRepository(Program::class)
+             ->findAll();
+
+         return $this->render(
+             'program/index.html.twig',
+             ['programs' => $programs]
+         );
     }
 
     /**
-     * @Route("{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
-     */
-    public function show(int $id): Response
+    * Getting a program by id
+    *
+    * @Route("/show/{id<^[0-9]+$>}", name="show")
+    * @return Response
+    */
+    public function show(int $id):Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+        ]);
     }
 
 }
